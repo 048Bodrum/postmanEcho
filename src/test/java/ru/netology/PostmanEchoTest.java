2c04b58b4a8a8f1aea868e7beb499a1b106b2996
Bilen Liubov <bilenliubov48@mail.ru>
2024-11-14 15:52:00 +0300

    initial commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,21 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+
+class PostmanechoTest {
+    @Test
+    void shouldReturnSendData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("netology") // отправляемые данные (заголовки и query можно выставлять аналогично)
+
+                .when().log().all()
+                .post("/post")
+                .then().log().all()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo("netology"));
+    }
+}
